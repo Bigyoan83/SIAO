2336f4edddb9322b36fc787e48b763e7816289a5
Bigyoan83 <163867092+Bigyoan83@users.noreply.github.com>
2024-09-24 12:05:04 +0200

    12h

---
+++ connexion/connexion.php
@@ -0,0 +1,14 @@
+<?php
+
+$serveur = "localhost";
+$utilisateur = "root";
+$motDePasse = "";
+$nomBaseDeDonnees = "mon_site_web";
+
+$connexion = new mysqli($serveur, $utilisateur, $motDePasse, $nomBaseDeDonnees);
+
+if ($connexion->connect_error) {
+    die("Connexion échouée : " . $connexion->connect_error);
+}
+
+?>
