b5a2bb47c2b5f0fdf379d4418f700af9fd4d0d8f
malteronie <153739321+malteronie@users.noreply.github.com>
2024-09-24 12:04:50 +0200

    connxion_php

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -2,7 +2,7 @@
 // Connexion à la base de données
 $serveur = "localhost";
 $utilisateur = "root";
-$motDePasse = "";
+$motDePasse = "root";
 $nomBaseDeDonnees = "mon_site_web";
 
 $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $nomBaseDeDonnees);
@@ -10,20 +10,18 @@ $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $nomBaseDeDonnees);
 if ($connexion->connect_error) {
     die("Connexion échouée : " . $connexion->connect_error);
 }
-
 // Requête SQL pour récupérer les utilisateurs
-$sql = "SELECT id, nom, email FROM utilisateurs";
+$email = 'jeff@gmail.com';
+$sql = "SELECT id, nom, mot_de_passe , email FROM utilisateur WHERE email = 'jeff@gmail.com'";
+
 $resultat = $connexion->query($sql);
-
 if ($resultat->num_rows > 0) {
-    // Afficher les résultats
-    while ($row = $resultat->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - Nom: " . $row["nom"] . " - Email: " . $row["email"] . "<br>";
-    }
-} else {
-    echo "Aucun utilisateur trouvé.";
+    if($_POST['email'] == $row["email"] && $_POST['mot_de_passe'] == $row["mot_de_passe"]){
+        echo ("Vous êtes bien connecté à l'utilisateur". $row['nom']);
+    } else{
+        return ("connexion echouée");
 }
-
+}
 // Fermer la connexion
 $connexion->close();
 ?>
