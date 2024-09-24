fe45de8a3929d0e78d5b86d9ad0e0e2bcc528f64
malteronie <153739321+malteronie@users.noreply.github.com>
2024-09-24 12:51:25 +0200

    Update connexion.php

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -11,18 +11,18 @@ if ($connexion->connect_error) {
     die("Connexion échouée : " . $connexion->connect_error);
 }
 // Requête SQL pour récupérer les utilisateurs
-$email = 'jeff@gmail.com';
-$sql = "SELECT id, nom, mot_de_passe , email FROM utilisateur WHERE email = 'jeff@gmail.com'";
+$email = $_POST['email'];
+
+$sql = "SELECT id, nom, mot_de_passe , email FROM utilisateur WHERE email = '$email'";
 // echo $sql;
 $resultat = $connexion->query($sql);
-
 
 $row = $resultat->fetch_assoc();
 
 
 if ($resultat->num_rows > 0) {
     if($_POST['email'] == $row["email"] && $_POST['mot_de_passe'] == $row["mot_de_passe"]){
-        echo ("Vous êtes bien connecté à l'utilisateur ". $row['mot_de_passe']);
+        echo ("Vous êtes bien connecté à l'utilisateur ". $row['nom']);
     } else{
         return ("connexion echouée");
 }
