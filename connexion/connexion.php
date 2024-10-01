efaaf0d484bc4b926d63ddec2b3a871443455573
Bigyoan83 <163867092+Bigyoan83@users.noreply.github.com>
2024-10-01 10:59:41 +0200

    commentaire

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -6,7 +6,9 @@ $email = $_POST['email'];
 $mot_de_passe = $_POST['mot_de_passe'];
 
 $sql = $connexion->prepare("SELECT id, nom, mot_de_passe, email FROM utilisateur WHERE email = ?");
+//Prépare une requête SQL. Cela signifie que la requête est écrite avec un emplacement réservé (?) pour éviter les injections SQL. Cela est plus sûr que d'insérer directement les variables dans la requête.
 $sql->bind_param("s", $email);
+//Associe la valeur de $email au ? dans la requête. Le "s" signifie que l'email est une chaîne de caractères (string).
 $sql->execute();
 $resultat = $sql->get_result();
 $row = $resultat->fetch_assoc();
@@ -16,7 +18,7 @@ if ($row) {
         // Stocker le nom dans la session
         $_SESSION['user_name'] = $row['nom'];
         $_SESSION['success_message'] = "Connexion réussie, bienvenue " . $row['nom'] . " !";
-        
+
         header('Location: ../home.php'); // Redirection vers la page d'accueil après connexion
         exit();
     } else {
