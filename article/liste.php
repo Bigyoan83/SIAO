7cb37a680e2b78fa0402590ef160314d12fd28d4
malteronie <153739321+malteronie@users.noreply.github.com>
2024-10-01 11:06:05 +0200

    liste

--- article/liste.php
+++ article/liste.php
@@ -1,8 +1,19 @@
 <?php
+
+include('../Connexion_BDD.php');
+
+
+$sql = $connexion->prepare("SELECT id, titre, content, nom, date_creation FROM article");
+
+$sql->execute();
+$resultat = $sql->get_result();
+$row = $resultat->fetch_assoc();
+
+
 if ($resultat->num_rows > 0) {
     // Afficher les résultats
     while ($row = $resultat->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - Nom: " . $row["nom"] . " - Email: " . $row["email"] .  " - Nom: " . $row["mot_de_passe"] . "<br>";
+        echo "ID: " . $row["id"] . " - titre: " . $row["titre"] . " - content: " . $row["content"] .  " - nom: " . $row["nom"] .  " - date_creation: " . $row["date_creation"] . "<br>";
     }
 } else {
     echo "Aucun utilisateur trouvé.";
