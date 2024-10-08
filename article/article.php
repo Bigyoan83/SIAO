bc50d7bdefaad2e5e9b2b9d3e6a2bce336585908
malteronie <153739321+malteronie@users.noreply.github.com>
2024-10-08 15:06:44 +0200

    maj

--- article/article.php
+++ article/article.php
@@ -10,6 +10,7 @@ if ($_SESSION['email']!='admin@gmail.com' && $_SESSION['user_name'] != 'admin'){
     <head>
         <link rel="stylesheet" type="text/css" href="../style.css"/>
         <link rel="stylesheet" type="text/css" href="../css/article.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <title> SIAO 83 </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, inital-scale=1.0">
