5541eae3c6ddc957f1f4765d01f0a5823debec14
Bigyoan83 <163867092+Bigyoan83@users.noreply.github.com>
2024-10-15 11:51:43 +0200

    15/10 11h51

--- home.php
+++ home.php
@@ -19,7 +19,7 @@ session_start();
                 <ul class="nav1">
                     <div>
                         <li class="bouton"><a href="home.php">Accueil</a></li>
-                        <li class="bouton"><a href="Service/Service.php">Nos Services</a></li>
+                        <li class="bouton"><a href="Service/le_SIAO.php">Qui sommes nous ?</a></li>
                         <li class="bouton">Gouvernance</li>
                         <li class="bouton">Le SIAO</li>
                         <li class="bouton"><a href="./actualites.php">Actualité</a></li>
