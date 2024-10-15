a780167776e5daf6752d9147db47acae66e335d3
Bigyoan83 <163867092+Bigyoan83@users.noreply.github.com>
2024-10-15 10:39:09 +0200

    modif nav

--- article/liste.php
+++ article/liste.php
@@ -27,7 +27,7 @@ if (isset($_GET['id'])) {
         </div>
         <ul>
             <li class="bouton"><a href="../home.php">Accueil</a></li>
-            <li class="bouton"><a href="../Service/Service.php">Nos Services</a></li>
+            <li class="bouton"><a href="../Service/le_SIAO.php">Qui sommes nous ?</a></li>
             <li class="bouton">Gouvernance</li>
             <li class="bouton">Le SIAO</li>
             <li class="bouton"><a href="../actualites.php">Actualité</a></li>
