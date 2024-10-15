798962436e07b26ee212320fcdeec1b226455fbd
Zakarayyy <164026262+Zakarayyy@users.noreply.github.com>
2024-10-15 10:40:25 +0200

    10h40 15-10-2024

--- home.php
+++ home.php
@@ -16,22 +16,24 @@ session_start();
                 <div class="profile">
                     <img src="images/SIAO.webp">
                 </div>
-                <ul>
-                    <li class="bouton"><a href="home.php">Accueil</a></li>
-                    <li class="bouton"><a href="Service/Service.php">Nos Services</a></li>
-                    <li class="bouton">Gouvernance</li>
-                    <li class="bouton">Le SIAO</li>
-                    <li class="bouton"><a href="./actualites.php">Actualité</a></li>
+                <ul class="nav1">
+                    <div>
+                        <li class="bouton"><a href="home.php">Accueil</a></li>
+                        <li class="bouton"><a href="Service/Service.php">Nos Services</a></li>
+                        <li class="bouton">Gouvernance</li>
+                        <li class="bouton">Le SIAO</li>
+                        <li class="bouton"><a href="./actualites.php">Actualité</a></li>
 
-                    <!-- Vérifiez si l'utilisateur est connecté -->
-                    <?php if (isset($_SESSION['user_name'])): ?>
-                        <!-- Si l'utilisateur est connecté, on affiche le bouton Mon Profil -->
-                        <li class="bouton"><a href="connexion/profil.php">Mon Profil (<?php echo htmlspecialchars($_SESSION['user_name']); ?>)</a></li>
-                    <?php else: ?>
-                        <!-- Sinon, on affiche le bouton de connexion -->
-                        <li class="bouton"><a href="connexion/connexion.html">Se Connecter/S'inscrire</a></li>
-                    <?php endif; ?>
-                </ul>   
+                        <!-- Vérifiez si l'utilisateur est connecté -->
+                        <?php if (isset($_SESSION['user_name'])): ?>
+                            <!-- Si l'utilisateur est connecté, on affiche le bouton Mon Profil -->
+                            <li class="bouton"><a href="connexion/profil.php">Mon Profil (<?php echo htmlspecialchars($_SESSION['user_name']); ?>)</a></li>
+                        <?php else: ?>
+                            <!-- Sinon, on affiche le bouton de connexion -->
+                            <li class="bouton"><a href="connexion/connexion.html">Se Connecter/S'inscrire</a></li>
+                        <?php endif; ?>
+                   </div>
+                </ul>  
             </nav>
 
 
