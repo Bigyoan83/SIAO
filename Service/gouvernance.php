acde92ccb471aac1d7fb66531fae77e131b61e65
Bigyoan83 <163867092+Bigyoan83@users.noreply.github.com>
2024-11-19 09:30:41 +0100

    police

--- Service/gouvernance.php
+++ Service/gouvernance.php
@@ -9,6 +9,7 @@ session_start();
         <link rel="stylesheet" type="text/css" href="/css/gouvernance.css"/>
         <link rel="stylesheet" type="text/css" href="/Service/gouvernance.css"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
         <title> SIAO 83 </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, inital-scale=1.0">
