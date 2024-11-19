6b753607f7205c2be79317604cdd922811697583
Bigyoan83 <163867092+Bigyoan83@users.noreply.github.com>
2024-11-19 09:08:47 +0100

    css gouvernance

--- Service/gouvernance.php
+++ Service/gouvernance.php
@@ -55,12 +55,14 @@ session_start();
                     <div class="intro-gouvernance">
                         <p>Opérateur de service public, le SIAO met en œuvre les orientations et décisions de l’Etat </p>
                     </div>
-                    <div class="paragraphe">
-                        <p>Une nouvelle gouvernance territoriale des SIAO est assurée au travers d’un Comité stratégique partenarial.Sous la présidence du préfet de département, cette instance associe l’ensemble des acteurs intervenant dans les parcours d’accès au logement,à l’hébergement et à l’accompagnement des personnes sans domicile ou mal logées </p>
-                    </div>
-                    <div class="paragraphe">
-                        <p>Les collectivités territoriales , les acteurs du champ de la santé , de l’emploi , les représentants des résidences sociales ,les bailleurs sociaux sont partie prenante  de cette gouvernance, disposant  de  compétences,de ressources et de leviers utiles et importants ; Le Comité stratégique partenarial permet de discuter collectivement de la stratégie pour la résorption du sans-abrismeou du mal logement sur le territoire, et surtout de sa mise en œuvre où chacun doit être impliqué et jouer son rôle.</p>
-                    </div>
+                    <div class="group-paragraphe">
+                        <div class="paragraphe">
+                            <p>Une nouvelle gouvernance territoriale des SIAO est assurée au travers d’un Comité stratégique partenarial.Sous la présidence du préfet de département, cette instance associe l’ensemble des acteurs intervenant dans les parcours d’accès au logement,à l’hébergement et à l’accompagnement des personnes sans domicile ou mal logées </p>
+                        </div>
+                        <div class="paragraphe">
+                            <p>Les collectivités territoriales , les acteurs du champ de la santé , de l’emploi , les représentants des résidences sociales ,les bailleurs sociaux sont partie prenante  de cette gouvernance, disposant  de  compétences,de ressources et de leviers utiles et importants ; Le Comité stratégique partenarial permet de discuter collectivement de la stratégie pour la résorption du sans-abrismeou du mal logement sur le territoire, et surtout de sa mise en œuvre où chacun doit être impliqué et jouer son rôle.</p>
+                        </div>
+                    </div> 
                 </div>
                 <div class="paragraphe">
                     <p>Le CSP permet au SIAO d’être doté d’un pilotage qui traduit l’articulation des politiques publiques entre elles et la responsabilité partagée de la gouvernance du SIAO est une co-responsabilité </p>
