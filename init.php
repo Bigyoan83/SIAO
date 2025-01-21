5d966ebb5d6f1ea4209325bf6006ae458f7b044d
Bigyoan83 <misecur@protonmail.com>
2025-01-21 12:17:56 +0100

    Update init.php

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+<<<<<<< Updated upstream
 \Sentry\init([
   'dsn' => 'https://fd3f9fdaae5ef40ba0008b5c1705a931@o4508359032700928.ingest.de.sentry.io/4508648038465616',
 ]);
@@ -11,3 +12,13 @@ try {
 } catch (\Throwable $exception) {
   \Sentry\captureException($exception);
 }
+=======
+Sentry\init([
+    'dsn' => 'https://fd3f9fdaae5ef40ba0008b5c1705a931@o4508359032700928.ingest.de.sentry.io/4508648038465616',
+    // Specify a fixed sample rate
+    'traces_sample_rate' => 1.0,
+    // Set a sampling rate for profiling - this is relative to traces_sample_rate
+    'profiles_sample_rate' => 1.0,
+    'logger' => new \Sentry\Logger\DebugFileLogger('./logfile.log'),
+  ]); 
+>>>>>>> Stashed changes
