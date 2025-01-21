2295cd7982258b1da8a2ebcf5641d3c14a4b0f1f
Bigyoan83 <163867092+Bigyoan83@users.noreply.github.com>
2025-01-21 09:55:42 +0100

    Update init.php

--- init.php
+++ init.php
@@ -2,11 +2,12 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-Sentry\init([
-    'dsn' => 'https://9770748e6476547680df98c12a4ebf15@o4508363333959680.ingest.de.sentry.io/4508363545575504',
-    // Specify a fixed sample rate
-    'traces_sample_rate' => 1.0,
-    // Set a sampling rate for profiling - this is relative to traces_sample_rate
-    'profiles_sample_rate' => 1.0,
-    'logger' => new \Sentry\Logger\DebugFileLogger('./logfile.log'),
-  ]); 
+\Sentry\init([
+  'dsn' => 'https://fd3f9fdaae5ef40ba0008b5c1705a931@o4508359032700928.ingest.de.sentry.io/4508648038465616',
+]);
+
+try {
+  $this->functionFailsForSure();
+} catch (\Throwable $exception) {
+  \Sentry\captureException($exception);
+}
